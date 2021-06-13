e59cad24861c2b497e95f4c9e7c6d5f1b9d174e6
betocoutinho <robertocoutinho2207@outlook.com>
2021-06-13 19:29:41 -0300

    Funcionalidade person Create finalizada

--- src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
+++ src/main/java/one/digitalinnovation/personapi/repository/PersonRepository.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.personapi.repository;
 
 import one.digitalinnovation.personapi.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,20 +1,25 @@
 package one.digitalinnovation.personapi.service;
 
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
-    private final PersonRepository personRepository;
+    private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savePerson = this.personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personSave = personMapper.toModel(personDTO);
+
+        Person savePerson = this.personRepository.save(personSave);
         return MessageResponseDTO.builder()
                 .message("Created person with ID: " + savePerson.getId())
                 .build();
